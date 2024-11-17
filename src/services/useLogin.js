4e29be504b6fea1c86acee6932029017859928c7
Yusi <yusi4781@naver.com>
2024-11-17 14:09:20 +0900

    fix: 로그인 리다이렉트 오류 수정

--- src/services/useLogin.js
+++ src/services/useLogin.js
@@ -1,23 +1,14 @@
-import { useEffect } from "react";
 import { useDispatch } from "react-redux";
 import { reduxLogin } from "../redux/authSlice";
 import { useNavigate } from "react-router-dom";
-import { useCookies } from "react-cookie";
 
 const useLogin = () => {
-  const navigate = useNavigate();
   const dispatch = useDispatch();
-  const [cookies] = useCookies(['access_token']);
-  const token = cookies.access_token;
-
-  useEffect(() => {
-    if (token) {
-      navigate('/')
-    }
-  }, [token, navigate])
+  const navigate = useNavigate();
 
   const login = async (email, password) => {
     dispatch(reduxLogin(email, password));
+    navigate('/');
   };
 
   return login;
